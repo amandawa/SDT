2c33d5af0a72219047a4443bdddc365cf50ec8b8
Amanda Wang <amandaaw23@gmail.com>
2017-11-30 12:59:35 -0500

    added bootstrap modal

--- preferences/Preferences.php
+++ preferences/Preferences.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <!-- <link rel="stylesheet" href="css/index.css" /> -->
     <script src = "https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/9.9.0/bootstrap-slider.js"></script>
@@ -333,7 +333,40 @@
 
         </ol>
 
-        <div class="button-group" id = "buttonContainer">
+   
+    
+        <!-- Trigger the modal with a button -->
+        <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal"  
+            style="width:70%;
+                height: 40px;
+                position:fixed;
+                bottom:0px;
+                text-align:center;
+                left: 15%;
+                font-size:16pt;
+            ">Save Preferences</button>
+      
+        <!-- Modal -->
+        <div class="modal fade" id="myModal" role="dialog">
+          <div class="modal-dialog">
+          
+            <!-- Modal content-->
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Preferences</h4>
+              </div>
+              <div class="modal-body">
+                <p>Saved Successfully.</p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+        <!-- <div class="button-group" id = "buttonContainer">
             <input type="submit" class="btn btn-info" value="Save Preferences" 
             style="width:70%;
                 height: 40px;
@@ -343,7 +376,7 @@
                 left: 15%;
                 font-size:16pt;
             ">
-        </div>
+        </div> -->
     </form>
 </div>
 <br><br><br>
